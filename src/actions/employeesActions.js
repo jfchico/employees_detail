1f9adec83a5dc702a30f781255af9b9efb60a5ca
Juan Francisco Chico Moreno <JuanF@MBP-de-Juan-2.home>
2019-07-30 19:29:59 +0200

    actions getting data from server with axios

--- src/actions/employeesActions.js
+++ src/actions/employeesActions.js
@@ -1,37 +1,33 @@
+import axios from 'axios';
+
+import {GET_EMPLOYEES_LIST_URL, GET_EMPLOYEE_DETAIL_URL} from '../api.endpoints';
 import { GET_EMPLOYEES_LIST, GET_EMPLOYEE_DETAIL } from '../actionsTypes';
 
 export const getEmployeesList = () => {
-  return {
-    type: GET_EMPLOYEES_LIST,
-    payload:
-      [
-        {
-          "id":"4343",
-          "employee_name":"Mrs. Brent Willms",
-          "employee_salary":"158200",
-          "employee_age":"27",
-          "profile_image":""
-        },
-        {
-          "id":"4346",
-          "employee_name":"Dr. Williams Junco",
-          "employee_salary":"15000",
-          "employee_age":"35",
-          "profile_image":""
-        },
-      ],
+  return dispatch => {
+    axios.get(GET_EMPLOYEES_LIST_URL).then(
+      res => {
+        dispatch (
+          {
+            type: GET_EMPLOYEES_LIST,
+            payload: res.data,
+          }
+        );
+      }
+    )
   };
 };
 export const getEmployeeDetail = id => {
-  return {
-    type: GET_EMPLOYEE_DETAIL,
-    payload:
-      {
-        "id":"4343",
-        "employee_name":"Mrs. Brent Willms",
-        "employee_salary":"158200",
-        "employee_age":"27",
-        "profile_image":"https://facebook.github.io/create-react-app/img/logo.svg"
-      },
+  return dispatch => {
+    axios.get(GET_EMPLOYEE_DETAIL_URL(id)).then(
+      res => {
+        dispatch (
+          {
+            type: GET_EMPLOYEE_DETAIL,
+            payload: res.data,
+          }
+        );
+      }
+    )
   };
 };
---
+++ src/api.endpoints.js
@@ -0,0 +1,2 @@
+export const GET_EMPLOYEES_LIST_URL = 'http://dummy.restapiexample.com/api/v1/employees';
+export const GET_EMPLOYEE_DETAIL_URL = id => `http://dummy.restapiexample.com/api/v1/employee/${id}`;
